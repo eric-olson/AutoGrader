461dd0c760eacfff2465dd27ec9ad2b987370a5b
Chris Rice <chrisrice93@gmail.com>
2016-05-23 15:00:31 -0600

    prepareTestEnvironment works

--- testing_tool/main.rb
+++ testing_tool/main.rb
@@ -1,10 +1,10 @@
 require 'tmpdir'
 require 'fileutils'
 
-assignment_path = ARGV[0]
-source_file_path = ARGV[1]
+# assignment_path = ARGV[0]
+# source_file_path = ARGV[1]
 
-common_dir = 'common'
+
 
 def prepareTestEnvironment(dir)
     # shutil.copy(CWD + '/tests.cpp', TEMPDIR);
@@ -13,12 +13,17 @@ def prepareTestEnvironment(dir)
     # shutil.copy(COMMONDIR + '/assoc.h', TEMPDIR);
     # shutil.copy(COMMONDIR + '/newtracker.cpp', TEMPDIR);
 
-    FileUtils.cp(assignment_path, File.join(dir, 'solution.cpp'))
-    FileUtils.cp(File.join(source_file_path, 'tests.cpp'), dir)
+    common_dir = 'common'
+    assignment_path = 'problems/anonymous/'
+    source_file_path = 'anonymous_solution.cpp'
+
+    FileUtils.cp(source_file_path, File.join(dir, 'solution.cpp'))
+    FileUtils.cp(File.join(assignment_path, 'tests.cpp'), dir)
     FileUtils.cp(File.join(common_dir, 'main.o'), dir)
     FileUtils.cp(File.join(common_dir, 'sighandler.o'), dir)
-    FileUtils.cp(File.join(common_dir, 'assoc.h'))
-    FileUtils.cp(File.join(common_dir, 'newtracker.cpp'))
+    FileUtils.cp(File.join(common_dir, 'assoc.h'), dir)
+    FileUtils.cp(File.join(common_dir, 'newtracker.cpp'), dir)
+    FileUtils.cp_r(File.join(common_dir, 'tmpenv'), dir)
 end
 
 def runTests(dir)
@@ -28,5 +33,7 @@ end
 Dir.mktmpdir('user') { |dir|
     prepareTestEnvironment(dir)
     FileUtils.chdir(dir)
+    puts `pwd`
+    puts `ls -lR`
     runTests(dir)
 }
