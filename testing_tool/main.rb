8ae57a2574542c38cadc9c6584bbd0b0693292af
Chris Rice <crice@mines.edu>
2016-05-24 07:53:16 -0600

    Cleanup more

--- testing_tool/main.rb
+++ testing_tool/main.rb
@@ -1,29 +1,22 @@
 require 'tmpdir'
 require 'fileutils'
 
-# assignment_path = ARGV[0]
-# source_file_path = ARGV[1]
-common_dir = 'common'
-assignment_path = 'problems/anonymous/'
-source_file_path = 'anonymous_solution.cpp'
-
 class TestEnvironment
-    attr_reader :temp_dir, :assignment_path, :source_path
+    attr_reader :temp_dir, :assignment_path, :source_path, :common_dir
 
     def initialize(temp_dir, assignment_path, source_path)
         @temp_dir = temp_dir
         @assignment_path = assignment_path
         @source_path = source_path
+        # Do this in config file instead
+        @common_dir = 'common'
 
         prepareTestEnvironment()
-        FileUtils.chdir(temp_dir)
-
+        enterTempDir()
         buildTests()
     end
 
     def prepareTestEnvironment
-        common_dir = 'common'
-
         FileUtils.cp(source_path, File.join(temp_dir, 'solution.cpp'))
         FileUtils.cp(File.join(assignment_path, 'tests.cpp'), temp_dir)
         FileUtils.cp(File.join(common_dir, 'main.o'), temp_dir)
@@ -33,7 +26,12 @@ class TestEnvironment
         FileUtils.cp_r(File.join(common_dir, 'tmpenv'), temp_dir)
     end
 
+    def enterTempDir
+        FileUtils.chdir(temp_dir)
+    end
+
     def buildTests
+        # Maybe replace with a makefile to make life easier??
         `g++ -g -rdynamic -o tests -I. -I/usr/local/include/StanfordCPPLib -std=c++11 -pthread tests.cpp main.o sighandler.o /usr/local/lib/gtest/libgtest.a /usr/local/lib/StanfordCPPLib/libStanfordCPPLib.a`
     end
 
@@ -48,10 +46,21 @@ class TestEnvironment
 
 end
 
+def showHelp
+    puts "Intended use:"
+    puts "    ./test_tool.rb <assignment_path> <source_file_path>"
+end
+
+if (ARGV.length != 2)
+    showHelp()
+    exit()
+end
+
+assignment_path = ARGV[0]
+source_file_path = ARGV[1]
+
 Dir.mktmpdir('user') { |dir|
-    test_environment = TestEnvironment.new(dir,
-                                           assignment_path,
-                                           source_file_path)
+    test_environment = TestEnvironment.new(dir, assignment_path, source_file_path)
     test_environment.run()
     puts test_environment.getTestDetailXML()
 }
