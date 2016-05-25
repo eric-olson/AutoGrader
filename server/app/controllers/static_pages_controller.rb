24879b7afd88aee417cf659d61c5649161f591c7
Chris Rice <chrisrice93@gmail.com>
2016-05-25 10:01:22 -0600

    ew.

--- server/app/controllers/static_pages_controller.rb
+++ server/app/controllers/static_pages_controller.rb
@@ -1,7 +1,36 @@
-class StaticPagesController < ApplicationController
-  def acetest
-  end
+require 'tempfile'
+require 'fileutils'
 
-  def ProblemPage
-  end
+class StaticPagesController < ApplicationController
+    def acetest
+    end
+
+    def ProblemPage
+
+    end
+
+    def testCode
+        editor_text = params[:editor_text]
+
+        # Create tempfile and write the editor text to it
+        source_file = Tempfile.new("solution")
+        source_filepath = source_file.path
+        source_file.write(editor_text)
+        source_file.close
+
+        testing_tool_path = "/home/crice/AutoGrader/testing_tool/"
+        problem_path = "problems/anonymous"
+        xml_result = ""
+        Process.fork {
+            FileUtils.chdir(testing_tool_path)
+            xml_result = `ruby test_tool.rb #{problem_path} #{source_filepath}`
+        }
+
+        Process.wait()
+
+        puts xml_result
+
+
+        render nothing: true
+    end
 end
