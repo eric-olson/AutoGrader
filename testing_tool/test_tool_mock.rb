46812849a8e83c0ccc210a88c75020512d08ea0d
Chris Rice <chrisrice93@gmail.com>
2016-06-01 10:25:28 -0600

    Fixed mock test tool

--- testing_tool/test_tool_mock.rb
+++ testing_tool/test_tool_mock.rb
@@ -1,6 +1,8 @@
 # Returns a canned xml test report from google test
 # Intended for use on non-linux development environments where the actual test tool does not work.
 
+require 'json'
+
 def showHelp
     puts "Intended use:"
     puts "    ruby test_tool.rb <assignment_path> <source_file_path> <common_file_path>"
@@ -14,9 +16,13 @@ end
 common_file_path = ARGV[2]
 
 sample_xml_file = File.open(File.join(common_file_path, 'sample_output.xml'))
-puts {
-  :gtest_xml_report => sample_xml_file.read,
+xml_data = sample_xml_file.read
+
+json_result = {
+  :gtest_xml_report => xml_data,
   :compile_errors => "",
   :runtime_errors => ""
 }.to_json
+
+puts json_result
 #puts '<WARNING> This has been generated from ' + $PROGRAM_NAME + ', it is not a real test report! </WARNING>'
