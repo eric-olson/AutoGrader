65e081bce26c30a74c827cfad2e5892b9649a07b
Eric Olson <eric.olson.411@gmail.com>
2016-06-02 11:47:03 -0600

    Updated layout for labs to show current user's email address

--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -11,6 +11,7 @@ class User < ActiveRecord::Base
       user.provider = auth.provider
       user.uid = auth.uid
       user.email = auth.info.email
+      user.multipass_id = auth.info.first_name
       user.password = Devise.friendly_token[0,20]
     end
   end
