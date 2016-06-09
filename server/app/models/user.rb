ea40a3818a46b4587fb9e162cecfecb8dfbab283
Eric Olson <eric.olson.411@gmail.com>
2016-06-09 11:59:07 -0600

    Added shibboleth to user model

--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :rememberable, :trackable,
-         :omniauthable, :omniauth_providers => [:google_oauth2]
+         :omniauthable, :omniauth_providers => [:google_oauth2, :shibboleth]
   has_many :grades
 
   def self.from_omniauth(auth)
