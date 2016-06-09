954163f078289da21f03bb5e5cdb6cd8ded875bd
Eric Olson <eric.olson.411@gmail.com>
2016-06-09 10:49:33 -0600

    Added shibboleth config

--- server/config/initializers/devise.rb
+++ server/config/initializers/devise.rb
@@ -244,6 +244,9 @@ Devise.setup do |config|
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
 
   config.omniauth :google_oauth2, "669901614573-b51a5uerk9jq1qva9f8fbe7s523cc9ti.apps.googleusercontent.com", "BSDQYzFzB8FloUYTc2VD56KM"
+  config.omniauth :shibboleth, {:uid_field => 'uid',
+                         :info_fields => {:name => 'displayName'},
+                  }
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
