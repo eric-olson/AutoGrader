ce59317807997e32b87804417cb83f98d9d6b070
Chris Rice <chrisrice93@gmail.com>
2016-06-16 09:44:40 -0600

    Added submission messages and fixed grading error when the test has errors

--- server/lib/assignment_grader.rb
+++ server/lib/assignment_grader.rb
@@ -164,19 +164,27 @@ class AssignmentGrader
     xml_parse = Nokogiri::Slop(xml_result)
     tests_array = xml_parse.xpath("//testcase")
 
-    passed_tests = 0
-    total_tests = 0
+    grade = 0
 
-    tests_array.each { |test|
-      failed = test.respond_to?(:failure)
-      if !failed
-        passed_tests += 1
-      end
+    if @test_results.hasCompileErrors? || @test_results.hasRuntimeErrors? || @test_results.testTimeout?
 
-      total_tests += 1
-    }
+      grade = 0
+    else
+      passed_tests = 0
+      total_tests = 0
+      tests_array.each { |test|
+        failed = test.respond_to?(:failure)
+        if !failed
+          passed_tests += 1
+        end
 
-    grade = (passed_tests.to_f / total_tests.to_f) * 10
+        total_tests += 1
+      }
+      grade = (passed_tests.to_f / total_tests.to_f) * 10
+
+    end
+
+
     puts "Grade: " + grade.to_s
     return grade
   end
